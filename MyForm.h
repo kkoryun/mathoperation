897ccdfa2494bebcf6a7d554ad186a76c44ed8ad
Koryun <koryun@live.com>
2015-12-24 04:04:22 +0300

    form 1.02

--- MyForm.h
+++ MyForm.h
@@ -1,6 +1,9 @@
 #pragma once
-
-namespace Проект2 {
+#include<string>
+#include<iostream>
+#include"calculator.h"
+namespace Проект2 
+{
 
 	using namespace System;
 	using namespace System::ComponentModel;
@@ -8,6 +11,7 @@ namespace Проект2 {
 	using namespace System::Windows::Forms;
 	using namespace System::Data;
 	using namespace System::Drawing;
+	using namespace std;
 
 	/// <summary>
 	/// Summary for MyForm
@@ -18,9 +22,7 @@ namespace Проект2 {
 		MyForm(void)
 		{
 			InitializeComponent();
-			//
-			//TODO: Add the constructor code here
-			//
+			basestring = new string();
 		}
 
 	protected:
@@ -34,11 +36,18 @@ namespace Проект2 {
 				delete components;
 			}
 		}
+	private: string * basestring;
+	private: calculator * calc;
 	private: System::Windows::Forms::DataGridView^  dataGridView1;
 	protected:
 	private: System::Windows::Forms::TextBox^  textBox1;
 	private: System::Windows::Forms::Label^  label1;
 	private: System::Windows::Forms::Label^  label2;
+	private: System::Windows::Forms::Button^  button1;
+	private: System::Windows::Forms::Button^  button2;
+	private: System::Windows::Forms::Label^  label3;
+	private: System::Windows::Forms::TextBox^  textBox2;
+	private: System::Windows::Forms::Label^  label4;
 
 	private:
 		/// <summary>
@@ -57,48 +66,108 @@ namespace Проект2 {
 			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
 			this->label1 = (gcnew System::Windows::Forms::Label());
 			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->button2 = (gcnew System::Windows::Forms::Button());
+			this->label3 = (gcnew System::Windows::Forms::Label());
+			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
+			this->label4 = (gcnew System::Windows::Forms::Label());
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView1))->BeginInit();
 			this->SuspendLayout();
 			// 
 			// dataGridView1
 			// 
-			this->dataGridView1->ColumnHeadersHeightSizeMode = System::Windows::Forms::DataGridViewColumnHeadersHeightSizeMode::AutoSize;
-			this->dataGridView1->Location = System::Drawing::Point(638, 30);
+			this->dataGridView1->AllowUserToAddRows = false;
+			this->dataGridView1->AllowUserToDeleteRows = false;
+			this->dataGridView1->AutoSizeColumnsMode = System::Windows::Forms::DataGridViewAutoSizeColumnsMode::AllCells;
+			this->dataGridView1->ColumnHeadersHeightSizeMode = System::Windows::Forms::DataGridViewColumnHeadersHeightSizeMode::DisableResizing;
+			this->dataGridView1->ColumnHeadersVisible = false;
+			this->dataGridView1->Location = System::Drawing::Point(481, 140);
 			this->dataGridView1->Name = L"dataGridView1";
-			this->dataGridView1->Size = System::Drawing::Size(240, 150);
+			this->dataGridView1->RowHeadersVisible = false;
+			this->dataGridView1->Size = System::Drawing::Size(109, 201);
 			this->dataGridView1->TabIndex = 0;
 			// 
 			// textBox1
 			// 
 			this->textBox1->Location = System::Drawing::Point(12, 40);
 			this->textBox1->Name = L"textBox1";
-			this->textBox1->Size = System::Drawing::Size(471, 20);
+			this->textBox1->Size = System::Drawing::Size(935, 20);
 			this->textBox1->TabIndex = 1;
+			this->textBox1->Text = L"0";
 			this->textBox1->TextChanged += gcnew System::EventHandler(this, &MyForm::textBox1_TextChanged);
 			// 
 			// label1
 			// 
 			this->label1->AutoSize = true;
-			this->label1->Location = System::Drawing::Point(12, 101);
+			this->label1->Location = System::Drawing::Point(12, 117);
 			this->label1->Name = L"label1";
-			this->label1->Size = System::Drawing::Size(35, 13);
+			this->label1->Size = System::Drawing::Size(0, 13);
 			this->label1->TabIndex = 2;
-			this->label1->Text = L"label1";
 			// 
 			// label2
 			// 
 			this->label2->AutoSize = true;
-			this->label2->Location = System::Drawing::Point(12, 167);
+			this->label2->Location = System::Drawing::Point(69, 231);
 			this->label2->Name = L"label2";
-			this->label2->Size = System::Drawing::Size(35, 13);
+			this->label2->Size = System::Drawing::Size(0, 13);
 			this->label2->TabIndex = 3;
-			this->label2->Text = L"label2";
+			// 
+			// button1
+			// 
+			this->button1->Location = System::Drawing::Point(198, 78);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(612, 23);
+			this->button1->TabIndex = 4;
+			this->button1->Text = L"SET";
+			this->button1->UseVisualStyleBackColor = true;
+			this->button1->Click += gcnew System::EventHandler(this, &MyForm::button1_Click);
+			// 
+			// button2
+			// 
+			this->button2->Location = System::Drawing::Point(15, 157);
+			this->button2->Name = L"button2";
+			this->button2->Size = System::Drawing::Size(79, 46);
+			this->button2->TabIndex = 5;
+			this->button2->Text = L"CALCULATE";
+			this->button2->UseVisualStyleBackColor = true;
+			this->button2->Click += gcnew System::EventHandler(this, &MyForm::button2_Click);
+			// 
+			// label3
+			// 
+			this->label3->AutoSize = true;
+			this->label3->Location = System::Drawing::Point(12, 88);
+			this->label3->Name = L"label3";
+			this->label3->Size = System::Drawing::Size(124, 13);
+			this->label3->TabIndex = 6;
+			this->label3->Text = L"Reverse Polish Notation ";
+			// 
+			// textBox2
+			// 
+			this->textBox2->Location = System::Drawing::Point(12, 114);
+			this->textBox2->Name = L"textBox2";
+			this->textBox2->ReadOnly = true;
+			this->textBox2->Size = System::Drawing::Size(935, 20);
+			this->textBox2->TabIndex = 7;
+			// 
+			// label4
+			// 
+			this->label4->AutoSize = true;
+			this->label4->Location = System::Drawing::Point(12, 231);
+			this->label4->Name = L"label4";
+			this->label4->Size = System::Drawing::Size(53, 13);
+			this->label4->TabIndex = 8;
+			this->label4->Text = L"RESULT:";
 			// 
 			// MyForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(985, 332);
+			this->ClientSize = System::Drawing::Size(985, 338);
+			this->Controls->Add(this->label4);
+			this->Controls->Add(this->textBox2);
+			this->Controls->Add(this->label3);
+			this->Controls->Add(this->button2);
+			this->Controls->Add(this->button1);
 			this->Controls->Add(this->label2);
 			this->Controls->Add(this->label1);
 			this->Controls->Add(this->textBox1);
@@ -111,7 +180,58 @@ namespace Проект2 {
 
 		}
 #pragma endregion
-	private: System::Void textBox1_TextChanged(System::Object^  sender, System::EventArgs^  e) {
+	private: System::Void textBox1_TextChanged(System::Object^  sender, System::EventArgs^  e) 
+	{
+
 	}
-	};
+	private: System::Void button1_Click(System::Object^  sender, System::EventArgs^  e)
+	{
+		textBox2->Text = "";
+		String^ s = textBox1->Text;
+
+		int size = s->Length;
+		string bs = "";
+		int i = 0;
+
+		//String^ -> string
+		while (size--)
+			bs += s[i++];
+		cout << bs << endl;// write bs
+		//-----------------------------------
+		*basestring = bs;
+		//
+		calc = new calculator("(" + bs + ")");
+		calc->Calculation();
+		string * str;
+		str = calc->Getrpn();
+		int rpnsize = calc->Getrpnsize();
+		
+		//write rpn
+		for (size_t i = 0; i < rpnsize; i++)
+			cout << str[i];
+		//array string -> String^
+		i = 0;
+		s = "";
+		for (size_t i = 0; i < rpnsize; i++)
+			for (size_t j = 0; j < str[i].length(); j++) 
+				s += Convert::ToChar(str[i][j]);
+				
+		
+		textBox2->Text += s;
+		int l = calc->Getvaluetable().Getcount();
+		dataGridView1->RowCount = l;
+		dataGridView1->ColumnCount = 2/*calc->Getvaluetable().Getcount()*/;
+		for (size_t i = 0; i < l; i++) {
+			dataGridView1->Rows[i]->Cells[0]->ReadOnly = true;
+			//dataGridView1->Rows[i]->Cells[0]->Value =Convert::ToString(calc->Getvaluetable().Getrecord(i).Getname() ;
+		}
+		
+	}
+private: System::Void button2_Click(System::Object^  sender, System::EventArgs^  e) {
+	calc = new calculator("(" + *basestring + ")");
+	double result = calc->Calculation();
+	cout << result << endl;
+	label2->Text = Convert::ToString(result);
+}
+};
 }
